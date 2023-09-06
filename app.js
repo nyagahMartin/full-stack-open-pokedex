5d0a421a62a8fb4e42e7d55dd20140d003f61d0f
menyagah <menyagah27@gmail.com>
2023-09-06 18:07:31 +0300

    change server address to 5001

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server running on port 5000')
+  console.log('server running on port 5001')
 })
