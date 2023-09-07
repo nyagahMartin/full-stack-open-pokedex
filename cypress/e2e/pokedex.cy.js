6653cbf7ec7433560fe3996be302a2b28c3d795b
menyagah <menyagah27@gmail.com>
2023-09-07 11:06:53 +0300

    Add test to validate pokemon page can be navigated to

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,3 +6,11 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Pokedex', function() {
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.contains('Chlorophyll')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
