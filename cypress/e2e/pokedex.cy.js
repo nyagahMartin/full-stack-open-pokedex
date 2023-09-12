70025f1333d53e79388edcd70722b5e2a0e50bf2
nyagahMartin <nyagahmartin30@gmail.com>
2023-09-12 14:54:34 +0300

    Fix failing cypress tests

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('https://twilight-haze-7938.fly.dev/')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
@@ -9,7 +9,7 @@ describe('Pokedex', function() {
 
 describe('Navigate', function() {
   it('pokemon page can be navigated to', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('https://twilight-haze-7938.fly.dev/')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
