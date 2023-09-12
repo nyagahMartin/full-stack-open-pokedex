4597951a4d90a43bbce38963946ca61e850979c3
nyagahMartin <nyagahmartin30@gmail.com>
2023-09-12 15:00:30 +0300

    Fix failing build job

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server is running on port 5000')
+  console.log('server is running on port 3000')
 })
